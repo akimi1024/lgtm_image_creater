4b6b9da264e33722cd4bc6bbe890fd6fb2830b3f
宮澤昭海 <>
2025-11-25 07:49:19 +0900

    検索キーワードから画像を取得

--- lgtm/image_source.py
+++ lgtm/image_source.py
@@ -15,7 +15,26 @@ class RemoteImage:
   def __init__(self, path):
     self._url = path
 
+
   def get_image(self):
     data = requests.get(self._url)
     # バイトデータをファイルオブジェクトに変換
     return BytesIO(data.content)
+
+
+class _LoremFlicker(RemoteImage):
+  """キーワード検索で画像を取得"""
+  LOREM_FLICKER_URL = 'https://loremflicker.com'
+  WIDTH = 800
+  HEIGHT = 600
+
+  def __init__(self, keyword):
+    super().__init__(self.build_url(keyword))
+
+
+  def _build_url(self, keyword):
+    return (f'{self.LOREM_FLICKER_URL}/'
+            f'{self.WIDTH}/{self.HEIGHT}/{keyword}')
+
+
+KeywordImage = _LoremFlicker
