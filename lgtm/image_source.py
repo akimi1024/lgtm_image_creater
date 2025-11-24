e3dc9c0199b53e914126c9349b9860bc15c29dec
宮澤昭海 <>
2025-11-25 07:40:22 +0900

    URLから画像を取得するクラスを追加

--- lgtm/image_source.py
+++ lgtm/image_source.py
@@ -1,3 +1,4 @@
+from io import BytesIO
 import requests
 
 class LocalImage:
@@ -7,3 +8,14 @@ class LocalImage:
 
   def get_image(self):
     return open(self._path, 'rb')
+
+class RemoteImage:
+  """URLから画像を取得する"""
+
+  def __init__(self, path):
+    self._url = path
+
+  def get_image(self):
+    data = requests.get(self._url)
+    # バイトデータをファイルオブジェクトに変換
+    return BytesIO(data.content)
